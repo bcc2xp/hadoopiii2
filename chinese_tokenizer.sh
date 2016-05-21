5f3bcd6fe98f0be3a5557d0b51889709d5664e37
David Chiu <tr.ywchiu@gmail.com>
2016-05-21 16:43:53 +0800

    update

--- chinese_tokenizer.sh
+++ chinese_tokenizer.sh
@@ -11,5 +11,8 @@ wget https://github.com/ywchiu/hadoopiii2/raw/master/reducer.py
 # 4. Execute Hadoop Streaming
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -file /home/cloudera/mapper.py -mapper /home/cloudera/mapper.py -file /home/cloudera/reducer.py -reducer /home/cloudera/reducer.py -input /user/cloudera/president/wc.txt -output /user/cloudera/wordout
 
-# 5. check ip: ifconfig
-# 6. <IP>:8888
+# 5. Test Script 
+python mapper.py < test.txt | sort -k 1 | python reducer.py
+
+# 6. check ip: ifconfig
+# 7. <IP>:8888
